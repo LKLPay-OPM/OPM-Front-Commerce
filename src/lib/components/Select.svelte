491ac84b29b0ed6b052cfcdf33e06360689b35b6
FDzGvn <fernando@atidyarok.com>
2022-12-09 01:47:07 -0600

    Onboarding (Pending Styles & save data to DB)

--- src/lib/components/Select.svelte
+++ src/lib/components/Select.svelte
@@ -1,23 +1,88 @@
 <script>
-    export let label = "";
-    export let id = "";
-    export let placeholder = "";
-    export let value = "";
+	export let label = "";
+	export let id = "";
+	export let placeholder = "";
+	export let value = "";
+	export let index = 0;
+	export let classList = "";
 
-    export let defaultText = "";
-    export let optionsList = [];
+	export let defaultText = "";
+	export let optionsList = [];
 
-    const onChange = e => (value = e.target.value);
+	$: {
+			value = optionsList[index]
+			// console.log(value)
+	}
+
+	const onChange = e => (value = e.target.value);
 </script>
 
-<div class="form-control">
-    <label class="label" for={id}>
-        <span class="label-text">{label}</span>
-    </label>
-    <select {placeholder} {id} on:change={onChange} class="input input-bordered">
-        <option value="" selected disabled hidden>{defaultText}</option>
-        {#each optionsList as item}
-            <option value={item.value}>{item.name}</option>
-        {/each}
-    </select>
+<div class="box">
+	<label class="label" for={id}>
+		<span class="label-text">{label}</span>
+	</label>
+	<select {placeholder} bind:value={index} {id}>
+		{#each optionsList as item, i}
+				<option value={i}>{item.name}</option>
+		{/each}
+	</select>
 </div>
+
+
+<style>
+
+	.box {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.label {
+		font-weight: 700;
+		font-size: .8125rem;
+		line-height: 1.25rem;
+		display: flex;
+		justify-content: left;
+		color: #6F7E8C;
+	}
+
+	.box select {
+		font-weight: 500;
+		font-size: .8125rem;
+		line-height: 1.125rem;
+		color: #8B9EB0;
+		height: 3rem;
+		width: 10.5rem;
+		padding-left: 1.25rem;
+		border: none;
+		outline: none;
+		background: linear-gradient(91.36deg, #EFEEF5 0%, #E6E8EF 100%);
+		box-shadow: inset -3px -3px 4px #F9FCFF, inset 3px 3px 3px #AEB8C0;
+		border-radius: 4px;
+	}
+
+	.box::before {
+  content: "\f13a";
+  font-family: FontAwesome;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 20%;
+  height: 100%;
+  text-align: center;
+  font-size: 28px;
+  line-height: 45px;
+  color: rgba(255, 255, 255, 0.5);
+  background-color: rgba(255, 255, 255, 0.1);
+  pointer-events: none;
+}
+	.box select option {
+		font-weight: 700;
+		font-size: 1rem;
+		line-height: 1.25rem;
+		color: #6F7E8C;
+		padding: 2rem;
+		margin: 2rem;
+		background: linear-gradient(91.36deg, #EFEEF5 0%, #E6E8EF 100%);
+	}
+</style>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,14 +1,150 @@
 <script>
-	import { isLoggedIn } from '$lib/stores';
+	import { isLoggedIn, loggedInUser } from '$lib/stores';
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 	import Loader from '$lib/components/Loader.svelte'
+	import Modal from '$lib/components/Modal.svelte';
+	import Select from '$lib/components/Select.svelte';
+	import Input from '$lib/components/Input.svelte';
+	import Icons from '$lib/components/Icons.svelte';
+	import noUser from '$lib/assets/no_user.png';
+
+	let modalOnboarding;
+	let selected;
+
+  $: {
+    // console.log(selected)
+  }
+
+	let options = [
+    {
+      value: "basic",
+      name: "Básico"
+    },
+    {
+      value: "pro",
+      name: "Profesional"
+    },
+    {
+      value: "physical",
+      name: "Persona Física"
+    },
+    {
+      value: "moral",
+      name: "Persona Moral"
+    }
+  ]
+
+	let userData = {
+		uid: $loggedInUser.uid,
+		name: "",
+		lastName: "",
+		curp: "",
+		accountType: "",
+		businessName: "",
+		businessLine: "",
+		businessAddress: "",
+		outsideNumber: "",
+		insideNumber: "",
+		zipCode: "",
+		state: "",
+		town: "",
+		suburb: "",
+		avatar: "",
+		phone: "",
+	}
+
+	
+
+	const handleUpdateProfile = () => {
+		console.log(userData)
+	}
+
+	const handleUpdateProfilePic = async() => {
+    const uid = userData.uid;
+    const picture = document.getElementById('file').files[0]
+    const fileType = document.getElementById('file').files[0].type;
+
+    let profileData = {};
+    profileData.uid = uid;
+    profileData.avatar = picture;
+    profileData.fileType = fileType;
+    // await updateUserAvatar(profileData)
+    // console.log(profileData)
+    profileData = {};
+  }
+
+	const show = (option) => {
+    option.show();
+  }
+
+  const closeModal = (option) => {  
+    option.closeModal();
+  }
 
 	onMount(() => {
+		if($loggedInUser.firstTimeUser == true){
+			show(modalOnboarding)
+		}
 			//console.log($page)
 	});
 </script>
+
+
+
 {#if $isLoggedIn === true}
+<Modal bind:this={modalOnboarding}>
+  <div slot="header">
+    <h1>Bienvenido a LKL Pay</h1>
+  </div>
+  <div slot="content">
+    <p>
+      Para verificar que realmente eres tu quien utilizara tu cuenta y brindarte la mejor seguridad,<br>
+			necesitamos los siguientes datos personales, siempre protegidos bajo nuestra <a href="/home#privacy">Política de Privacidad</a>
+    </p>
+		<div class="avatar-container">
+			<div class="avatar">
+				<img class="avatar-img" alt="imagen" src="{$loggedInUser.avatar ? $loggedInUser.avatar : noUser}" />
+				<label for="file">
+					<input id="file" type="file" on:change={handleUpdateProfilePic} accept="image/jpeg, image/png"/>
+					<div class="avatar-content">
+						<span class="avatar-icon"><Icons name="camera-fill" width="24" height="24"/></span>
+						<span class="avatar-text">Cambiar Foto</span>
+					</div>
+				</label>
+			</div>
+		</div>
+		<Select bind:value={userData.accountType} label="Tipo de cuenta" defaultText={"Elige una opción"} bind:optionsList={options}/>
+    <Input label="Nombre(s)" id="name" bind:value={userData.name} className="txt-field normal" type="text"/>
+    <Input label="Apellidos(s)" id="lastName" bind:value={userData.lastName} className="txt-field normal" type="text"/>
+    <Input label="Nombre del Negocio" id="businessName" bind:value={userData.businessName} className="txt-field normal" type="text"/>
+    <Input label="Giro" id="businessLine" bind:value={userData.businessLine} className="txt-field normal" type="text"/>
+    <Input label="Dirección con Número" id="businessAddress" bind:value={userData.businessAddress} className="txt-field normal" type="text"/>
+    <Input label="Número Exterior" id="outsideNumber" bind:value={userData.outsideNumber} className="txt-field normal" type="text"/>
+    <Input label="Número Interior" id="insideNumber" bind:value={userData.insideNumber} className="txt-field normal" type="text"/>
+    <Input label="Código Postal" id="zipCode" bind:value={userData.zipCode} className="txt-field normal" type="text"/>
+    <Input label="Estado" id="state" bind:value={userData.state} className="txt-field normal" type="text"/>
+    <Input label="Municipio" id="town" bind:value={userData.town} className="txt-field normal" type="text"/>
+    <Input label="Colonia" id="suburb" bind:value={userData.suburb} className="txt-field normal" type="text"/>
+    <Input label="Teléfono" id="phone" bind:value={userData.phone} className="txt-field normal" type="text"/>
+  </div>
+  <div class="modal-buttons" slot="footer">
+    <Input on:click={closeModal(modalOnboarding)} label="Cerrar" id="buttonCloseModalBankInfo" type="button" className="button" icon=""/>
+    <Input 
+      on:click={closeModal(modalOnboarding)} 
+      on:click={() => handleUpdateProfile()}
+      label="Guardar" 
+      id="buttonSaveModalBankInfo" 
+      type="button" 
+      className={`button 
+        ${
+          ""
+        }`
+      } 
+      icon=""
+    />
+  </div>
+</Modal>
 <div class="content">
 	<div class="title">
 		<h1 class="text-8xl">Home</h1>
@@ -28,5 +164,64 @@
 .title{
   display: flex;
   justify-content: center;
+}
+
+.avatar-container {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: .5rem;
+}
+.avatar {
+  position: relative;
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  overflow: hidden;
+  background-color: #111;
+}
+.avatar img {
+  height: 100%;
+  width: 100%;
+  border-radius: 50%;
+  object-fit: cover;
+  justify-content: center;
+  cursor: pointer;
+}
+.avatar:hover .avatar-img {
+  opacity: .5;
+}
+.avatar-content {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  opacity: 0;
+  transition: opacity .2s ease-in-out;
+}
+.avatar:hover .avatar-content {
+  opacity: 1;
+  cursor: pointer;
+}
+.avatar-text {
+  text-transform: uppercase;
+  font-size: 12px;
+  width: 50%;
+  text-align: center;
+  cursor: pointer;
+}
+
+.avatar-icon {
+  color: white;
+}
+.avatar input {
+  display: none;
 }
 </style>
