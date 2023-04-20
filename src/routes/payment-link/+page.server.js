cbe02bc74de00150e537fe4fa4e488efe064b8f3
FDzGvn <fernando@atidyarok.com>
2023-04-20 12:38:20 -0500

    Cleaning code payment link server

--- src/routes/payment-link/+page.server.js
+++ src/routes/payment-link/+page.server.js
@@ -17,11 +17,5 @@ export const actions = {
       } catch (err) {
         throw new error(response?.status ?? 400, { info: response?.data?.message ?? "Something went wrong!" });
     }
-    const email = formData.get("email");
-    if (!email) {
-      return error(400, { email, missing: true });
-    }
-
-    return { success: true };
   },
 };
