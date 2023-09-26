f4e7b6c03fcee4f7aece1c483857df11764fb3a5
Armando Velasco <armando.velasco@lklpay.com.mx>
2023-09-26 13:09:41 -0600

    port to transaction

--- src/lib/constants/envs.js
+++ src/lib/constants/envs.js
@@ -1,5 +1,5 @@
 export const PUBLIC_PROFILES_ENDPOINT = 'https://boostdistro.lklpay.com.mx:50102/api'
 export const PUBLIC_DEVICES_ENDPOINT = 'https://boostdistro.lklpay.com.mx:50103/api'
-export const PUBLIC_FRAUD_PREVENTION_MANAGEMENT_ENDPOINT = 'https://transactions.lklpay.com.mx:50102/api'
+export const PUBLIC_FRAUD_PREVENTION_MANAGEMENT_ENDPOINT = 'https://transactions.lklpay.com.mx:50101/api'
 export const PUBLIC_DEPOSITS_AND_FEES_ENDPOINT = 'https://boostdistro.lklpay.com.mx:50104/api'
 export const PUBLIC_TICKETS_ENDPOINT = 'https://backoffice.lklpay.com.mx:50106/api'
