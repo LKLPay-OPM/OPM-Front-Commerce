9eef39a2e1c8617392fee791ab78ed62d2b4c82c
FDzGvn <fernando@atidyarok.com>
2023-11-06 13:49:43 -0600

    Fix tables

--- src/lib/utils/date.js
+++ src/lib/utils/date.js
@@ -35,6 +35,11 @@ export const timeToLocalString = (string) => {
   let str = string.replace(pattern, `$1:$2`);
   return str;
 };
+export const getTransactionTime = (string) => {
+  var pattern = /(\d{2})(\d{2})(\d{2})/; // String pattern replace for date
+  let str = string.replace(pattern, `$1:$2:$3`);
+  return str;
+};
 
 export function parseSlashDate(str) {
   return str.replace(/-/g, "/");
@@ -61,7 +66,9 @@ export function getStringDate(date) {
 }
 
 export const getMonthPeriod = (string) => {
-  var pattern = /(\d{2})(\d{2})/; // String pattern replace for date
+  // String pattern replace for date
+  if (string.length === 4) var pattern = /(\d{2})(\d{2})/;
+  if (string.length === 6) var pattern = /(\d{2})(\d{2})(\d{2})/;
   const extractMonth = string.replace(pattern, "$2");
   const month = getMonthName(Number(extractMonth) - 1);
   let str = string.replace(pattern, `${month} 20$1`);
