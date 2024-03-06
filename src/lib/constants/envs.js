15a328be3df7892d0543cbc2e2aaaaa94464f2b5
Armando Velasco <armando.velasco@lklpay.com>
2024-03-06 11:06:31 -0600

    email patter

--- src/lib/constants/envs.js
+++ src/lib/constants/envs.js
@@ -1,5 +1,5 @@
-export const PUBLIC_PROFILES_ENDPOINT = "https://devpddf.lklpay.com.mx:3000/api";
-export const PUBLIC_DEVICES_ENDPOINT = "https://devpddf.lklpay.com.mx:3001/api";
-export const PUBLIC_FRAUD_PREVENTION_MANAGEMENT_ENDPOINT = "https://devtransactions.lklpay.com.mx:3004/api";
-export const PUBLIC_DEPOSITS_AND_FEES_ENDPOINT = "https://devpddf.lklpay.com.mx:3006/api";
-export const PUBLIC_TICKETS_ENDPOINT = "https://devbt.lklpay.com.mx:3007/api";
+export const PUBLIC_PROFILES_ENDPOINT = 'https://boostdistro.lklpay.com.mx:50102/api';
+export const PUBLIC_DEVICES_ENDPOINT = 'https://boostdistro.lklpay.com.mx:50103/api';
+export const PUBLIC_FRAUD_PREVENTION_MANAGEMENT_ENDPOINT = 'https://transactions.lklpay.com.mx:50101/api';
+export const PUBLIC_DEPOSITS_AND_FEES_ENDPOINT = 'https://boostdistro.lklpay.com.mx:50104/api';
+export const PUBLIC_TICKETS_ENDPOINT = 'https://backoffice.lklpay.com.mx:50106/api';
