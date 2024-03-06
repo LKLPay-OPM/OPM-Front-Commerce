b658a72c1bfcc173a83c0f65c3a15b21e5a0c69e
Armando Velasco <armando.velasco@lklpay.com>
2024-03-06 11:01:14 -0600

    email pattern

--- src/lib/constants/pattern.js
+++ src/lib/constants/pattern.js
@@ -1,2 +1,2 @@
-export const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+export const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
 export const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$/;
