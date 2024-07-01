90025077fcd3ecd8a3dfcfc6132cdb9b1aa2d73b
FDzGvn <fernando@atidyarok.com>
2024-07-01 13:38:02 -0600

    Log update pass

--- src/routes/update-pass/[uuid]/+page.svelte
+++ src/routes/update-pass/[uuid]/+page.svelte
@@ -43,6 +43,7 @@
       });
       menu = "success";
     } catch (e) {
+      console.log(e)
       menu = "error";
       const handler = await appErrorResponseHandler(e);
       const code = handler?.code ?? 500;
