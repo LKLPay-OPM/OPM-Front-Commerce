d8f1980c34ee32e20b7e53d332300223634b145f
Your Name <you@example.com>
2025-01-10 15:59:15 -0600

    First Commit

--- src/lib/constants/envs.js
+++ src/lib/constants/envs.js
@@ -1,5 +1,5 @@
-export const PUBLIC_PROFILES_ENDPOINT = 'https://transactions.lklpay.com.mx:50102/api';
-export const PUBLIC_DEVICES_ENDPOINT = 'https://transactions.lklpay.com.mx:50103/api';
-export const PUBLIC_FRAUD_PREVENTION_MANAGEMENT_ENDPOINT = 'https://transactions.lklpay.com.mx:50101/api';
-export const PUBLIC_DEPOSITS_AND_FEES_ENDPOINT = 'https://transactions.lklpay.com.mx:50104/api';
-export const PUBLIC_TICKETS_ENDPOINT = 'https://backoffice.lklpay.com.mx:50106/api';
+export const PUBLIC_PROFILES_ENDPOINT = "https://boost.baz.viergegroup.com/pef1d7972c8ro/api";
+export const PUBLIC_DEVICES_ENDPOINT = "https://boost.baz.viergegroup.com/d4c625d83e5ev/api";
+export const PUBLIC_FRAUD_PREVENTION_MANAGEMENT_ENDPOINT = "https://boost.baz.viergegroup.com/f2c65bd1289pm/api";
+export const PUBLIC_DEPOSITS_AND_FEES_ENDPOINT = "https://boost.baz.viergegroup.com/d44106ff407ep/api";
+export const PUBLIC_TICKETS_ENDPOINT = "https://boost.baz.viergegroup.com/b4439701224ak/api";
