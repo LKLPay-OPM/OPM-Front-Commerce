175f62bff4d49ec43c24e1a7c9e496d924ab7d12
danielrussocki <danielrojas1032@gmail.com>
2025-05-30 16:13:43 -0600

    FIX - Ajuste

--- src/lib/utils/format.js
+++ src/lib/utils/format.js
@@ -2,3 +2,16 @@ export const formatDecimals = (num, decimals) => num.toLocaleString('es-MX', {
   minimumFractionDigits: 2,
   maximumFractionDigits: 2,
 })
+
+export const formatCardType = (string) => {
+  if (string === "credit") {
+    return "CRÉDITO";
+  }
+  if (string === "debit") {
+    return "DÉBITO";
+  }
+  if (string === "international") {
+    return "INTERNACIONAL";
+  }
+  return "N/A";
+};
